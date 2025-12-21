ee55a30551fa01eb9ae922ecbe4523d4d5eea83f
Harshitha Teki <tekiharshitha838@gmail.com>
2025-12-21 20:27:39 +0530

    Time: 0 ms (100%), Space: 50.3 MB (58.68%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int>a;
+        for(int i=0; i<order.size(); i++){
+            for(int j=0; j<friends.size(); j++){
+                if(order[i] == friends[j]){
+                    a.push_back(order[i]);
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+};
