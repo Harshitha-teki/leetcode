046da65477cf4f787021a6e000a6fc3b559d4359
Harshitha Teki <tekiharshitha838@gmail.com>
2026-02-20 18:57:55 +0530

    Time: 2 ms (25.01%), Space: 20.7 MB (24.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>mp;
-        int count;
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
+        int res =0;
+        for(int i: nums){
+            res = res ^ i;
         }
-        for(auto&i: mp){
-            if(i.second ==1){
-                count = i.first;
-            }
-        }
-        return count;
+        return res;
     }
 };
